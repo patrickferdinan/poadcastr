e653f760e33803fc19e633584d840383ec393623
patrickferdinan <patrick.ferdinan@hotmail.com>
2021-05-31 19:00:37 -0300

    consuming API on Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,45 @@
+import { useEffect } from "react";
 
 
-export default function Home() {
+export default function Home(props) {
+  //SPA
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/episodes')
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))
+  // }, [])
+
+  //SSR
+  //console.log(props.episodes);
+
+  //SSG
+
+  
   return (
     <h1>Index</h1>
   )
+}
+//SSR
+// export async function getServerSideProps() {
+//   const response = await fetch('http://localhost:3333/episodes');
+//   const data = await response.json();
+
+//   return {
+//     props: {
+//       episodes: data,
+//     }
+//   }
+// }
+
+//SSG
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
